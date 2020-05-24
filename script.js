daf3f1cca9af3f32718f0c9ecd044c6b0efcb464
sheready <csstlw@gmail.com>
2020-05-24 15:59:35 +0300

    add append method

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ $(document).ready(function(){
     $("#btn3").show();
   });
 });
+$(document).ready(function(){
+  $(".checkbox").click(function(){
+    $("#small").append(" <b>Ksh:200</b>.");
+    });  
+  $(".checkbox").click(function(){
+    $("#medium").append(" <b>Ksh:700</b>.");
+    
+  });  
+  $(".checkbox").click(function(){
+    $("#large").append(" <b>Ksh:1200</b>.");
+ });  
+ $(".checkbox").click(function(){
+  $("#crispy").append(" <b>Ksh:200</b>.");
+});
+$(".checkbox").click(function(){
+  $("#stuffed").append(" <b>Ksh:400</b>.");
+});
+$(".checkbox").click(function(){
+  $("#glutenfree").append(" <b>Ksh:800</b>.");
+});
+$(".checkbox").click(function(){
+  $("#pepperoni").append(" <b>Ksh:200</b>.");
+});
+$(".checkbox").click(function(){
+  $("#sausage").append(" <b>Ksh:600</b>.");
+});
+$(".checkbox").click(function(){
+  $("#mushroom").append(" <b>Ksh:800</b>.");
+});
+
+
+  
+});
