271a3b1863c4e0f03b4243804996c8c678b01f4d
sheready <csstlw@gmail.com>
2020-05-24 13:15:17 +0300

    add location  aler

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  function formRefresh(){
+    document.getElementById("form_data").reset();
+  }
+});
+$(document).ready(function(){
+ $('#btn1').click(function() {
+  alert("Your location is " + $("#location2").val());
+
+ });
+});
